d18ac719905f87ff31d8af5b6328cadfd67b6aca
JamesHD001 <henry.jameshd@gmail.com>
2026-09-06 04:27:32 +0100

    Add protected messages route shell

---
+++ web/app/messages/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/components/community/ComingSoon";
+
+export default function MessagesPage() {
+  return <ComingSoon title="Messages" description="Private one-to-one messaging will be connected here with server-enforced permissions, block barriers, and realtime delivery." />;
+}
