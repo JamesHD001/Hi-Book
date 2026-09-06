750cd489804f80e53efd1380ced4f8c7039cb077
JamesHD001 <henry.jameshd@gmail.com>
2026-09-06 04:27:27 +0100

    Add protected discover route shell

---
+++ web/app/discover/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/components/community/ComingSoon";
+
+export default function DiscoverPage() {
+  return <ComingSoon title="Discover" description="Global people discovery will be connected here, respecting privacy, blocks, moderation, and your discovery preferences." />;
+}
