c184096555c5b2c4fa291b8537bcd638ce6006f5
Henry Daniel James <henry.jameshd@gmail.com>
2026-09-22 17:25:17 +0100

    feat: add Hi!Book core UI primitives

---
+++ web/components/ui/HBCard.tsx
@@ -0,0 +1,5 @@
+import type { HTMLAttributes } from "react";
+
+export default function HBCard({ className="", ...props }: HTMLAttributes<HTMLDivElement>) {
+  return <div className={`hb-card ${className}`} {...props} />;
+}
