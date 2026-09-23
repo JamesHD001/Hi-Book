2bbe6038408e12e5cddf4ac30f7408cc0327b377
Henry Daniel James <henry.jameshd@gmail.com>
2026-09-23 15:08:27 +0100

    refactor(ui): normalize shared primitive formatting

--- web/components/ui/HBInput.tsx
+++ web/components/ui/HBInput.tsx
@@ -1,5 +1,8 @@
 import type { InputHTMLAttributes } from "react";
 
-export default function HBInput({ className="", ...props }: InputHTMLAttributes<HTMLInputElement>) {
+export default function HBInput({
+  className = "",
+  ...props
+}: InputHTMLAttributes<HTMLInputElement>) {
   return <input className={`hb-input ${className}`} {...props} />;
 }
